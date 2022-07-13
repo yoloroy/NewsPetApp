342c80e7912081bb467f7637a2ea9caf2f197900
yoloroy <slart2108@gmail.com>
2022-07-13 21:03:28 +0300

    :alien: fix nullable fields

--- domain/src/main/java/com/yoloroy/domain/model/NewsShort.kt
+++ domain/src/main/java/com/yoloroy/domain/model/NewsShort.kt
@@ -5,13 +5,13 @@ import java.util.*
 interface NewsShort {
     val title: String
     val description: String
-    val imageUrl: String
+    val imageUrl: String?
     val publicationDate: Calendar
 
     data class Base(
         override val title: String,
         override val description: String,
-        override val imageUrl: String,
+        override val imageUrl: String?,
         override val publicationDate: Calendar
     ) : NewsShort
 }
