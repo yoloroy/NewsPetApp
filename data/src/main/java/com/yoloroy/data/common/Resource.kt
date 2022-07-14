33cb9bc61fa907f585a3dbee030936908a0a33c2
yoloroy <slart2108@gmail.com>
2022-07-14 08:59:07 +0300

    :construction: add HttpError to Resource

--- data/src/main/java/com/yoloroy/data/common/Resource.kt
+++ data/src/main/java/com/yoloroy/data/common/Resource.kt
@@ -7,11 +7,13 @@ sealed class Resource<T> {
     sealed class Error<T> : Resource<T>()
 
     class NoInternetError<T> : Error<T>()
+    class HttpError<T> : Error<T>()
     class UnknownError<T> : Error<T>()
 
     fun <R> map(mapper: (T) -> R) = when(this) {
         is Success -> Success(mapper(data))
         is NoInternetError -> NoInternetError()
+        is HttpError -> HttpError()
         is UnknownError -> UnknownError()
     }
 }
