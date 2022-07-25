37ae4b07e725001041aa9ec481116ac2f958ca81
yoloroy <slart2108@gmail.com>
2022-07-25 20:48:29 +0300

    :bug: fix date presenting and add tests for presenting cases

--- app/src/main/java/com/yoloroy/newsapp/ui/mapper/common/CalendarToStringMapper.kt
+++ app/src/main/java/com/yoloroy/newsapp/ui/mapper/common/CalendarToStringMapper.kt
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class CalendarToStringMapper(locale: Locale) : Mapper<Calendar, String> {
-    private val format = SimpleDateFormat("dd MMMMMMMMM yyyy, HH:mm", locale)
+    private val format = SimpleDateFormat("d MMMM yyyy, HH:mm", locale)
 
     override fun map(data: Calendar): String = format.format(data.time)
 }
--- app/src/test/java/com/yoloroy/newsapp/ui/mapper/CalendarToStringMapperTest.kt
+++ app/src/test/java/com/yoloroy/newsapp/ui/mapper/CalendarToStringMapperTest.kt
@@ -24,4 +24,50 @@ class CalendarToStringMapperTest {
             assertEquals("18 $mName 2022, 22:22", mapper.map(date))
         }
     }
+
+    @Test
+    fun `check day presenting`() {
+        val mapper = CalendarToStringMapper(Locale.ENGLISH)
+        run {
+            val date = Calendar.getInstance().apply {
+                set(2022, 0, 18, 22, 22)
+            }
+            assertEquals("18 January 2022, 22:22", mapper.map(date))
+        }
+        run {
+            val date = Calendar.getInstance().apply {
+                set(2022, 0, 8, 22, 22)
+            }
+            assertEquals("8 January 2022, 22:22", mapper.map(date))
+        }
+    }
+
+    @Test
+    fun `check time presenting`() {
+        val mapper = CalendarToStringMapper(Locale.ENGLISH)
+        run {
+            val date = Calendar.getInstance().apply {
+                set(2022, 0, 18, 22, 22)
+            }
+            assertEquals("check normal time", "18 January 2022, 22:22", mapper.map(date))
+        }
+        run {
+            val date = Calendar.getInstance().apply {
+                set(2022, 0, 18, 2, 22)
+            }
+            assertEquals("check one symbol hour", "18 January 2022, 02:22", mapper.map(date))
+        }
+        run {
+            val date = Calendar.getInstance().apply {
+                set(2022, 0, 18, 22, 2)
+            }
+            assertEquals("check one symbol minute", "18 January 2022, 22:02", mapper.map(date))
+        }
+        run {
+            val date = Calendar.getInstance().apply {
+                set(2022, 0, 18, 2, 2)
+            }
+            assertEquals("check one symbol hour and minute", "18 January 2022, 02:02", mapper.map(date))
+        }
+    }
 }
